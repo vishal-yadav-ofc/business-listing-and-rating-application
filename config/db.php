5b6f7a3f8ecc0d40c48782e74d8b0eaba49645b5
Vishal Yadav <vishalbharatyadav.ofc@gmail.com>
2026-04-19 14:25:05 +0530

    schema name change as this will be picked up while starting application

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "business_system");
+$conn = new mysqli("localhost", "root", "", "business-listing-and-rating-application");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
