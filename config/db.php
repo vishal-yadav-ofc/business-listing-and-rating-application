da9e7154617ce681b3e8764cbf3b1cf4e3e6bd8d
Vishal Yadav <vishalbharatyadav.ofc@gmail.com>
2026-04-19 14:35:06 +0530

    schema name change reverted

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "business-listing-and-rating-application");
+$conn = new mysqli("localhost", "root", "", "business_system");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
